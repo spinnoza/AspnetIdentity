b1ef1aa94de568e2612a3c9407b688257c73ee68
Leo Wang <wangjil@delawareconsulting.com>
2023-01-10 17:57:45 +0800

    Identity 集成Entityframework

---
+++ AspnetIdentityWithEntityframework/Program.cs
@@ -0,0 +1,52 @@
+using System;
+using AspnetIdentityWithEntityframework.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddDbContext<AppDbContext>(config =>
+{
+    config.UseInMemoryDatabase("Memory");
+});
+
+// AddIdentity registers the services
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(config =>
+    {
+        config.Password.RequiredLength = 4;
+        config.Password.RequireDigit = false;
+        config.Password.RequireNonAlphanumeric = false;
+        config.Password.RequireUppercase = false;
+    })
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(config =>
+{
+    config.Cookie.Name = "Identity.Cookie";
+    config.LoginPath = "/Home/Login";
+});
+
+
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ AspnetIdentityWithEntityframework/readme.md
@@ -0,0 +1,41 @@
+## Identity 集成Entityframework
+
+- 配置中间件
+
+~~~ c#
+builder.Services.AddDbContext<AppDbContext>(config =>
+{
+    config.UseInMemoryDatabase("Memory");
+});
+
+// AddIdentity registers the services
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(config =>
+    {
+        config.Password.RequiredLength = 4;
+        config.Password.RequireDigit = false;
+        config.Password.RequireNonAlphanumeric = false;
+        config.Password.RequireUppercase = false;
+    })
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(config =>
+{
+    config.Cookie.Name = "Identity.Cookie";
+    config.LoginPath = "/Home/Login";
+});
+~~~
+
+
+
+- 注意下面这个不需要配置了
+
+~~~ c#
+builder.Services.AddAuthentication("CookieAuth")
+    .AddCookie("CookieAuth", config =>
+    {
+        config.Cookie.Name = "Grandmas.Cookie";
+        config.LoginPath = "/Home/Authenticate";
+    });
+~~~
+
